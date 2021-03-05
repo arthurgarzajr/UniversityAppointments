1e45b2cf46f2e9fbf8aab7013763c37f40e730d0
Arthur Garza <arthur.garzajr@gmail.com>
2021-03-04 21:13:04 -0600

    Initial Commit

---
+++ University/UniversityApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversityApp.swift
+//  University
+//
+//  Created by Arthur Garza on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct UniversityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
